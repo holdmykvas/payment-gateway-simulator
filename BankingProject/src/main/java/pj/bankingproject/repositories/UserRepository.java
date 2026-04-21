4a15073c27568058c4af4a482fe7f703a5b1bb9e
Nikita Pohrebniak <nikitanerf540@gmail.com>
2026-04-21 12:01:28 +0200

    LogIn through password

---
+++ BankingProject/src/main/java/pj/bankingproject/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package pj.bankingproject.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pj.bankingproject.entities.User;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
--- BankingProject/src/main/resources/data.sql
+++ BankingProject/src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO users (first_name, last_name, email) VALUES ('Nikita', 'Dev', 'nikita@bank.com');
-INSERT INTO users (first_name, last_name, email) VALUES ('Tech', 'Lead', 'lead@bank.com');
+INSERT INTO users (first_name, last_name, email, password) VALUES ('Nikita', 'Dev', 'nikita@bank.com', '$2a$12$ROA3s8qioaF/xR.L.56NceUOfL1i8tq.O57G8r4R4/s/m82K86tE2');
+INSERT INTO users (first_name, last_name, email, password) VALUES ('Tech', 'Lead', 'lead@bank.com', '$2a$12$ROA3s8qioaF/xR.L.56NceUOfL1i8tq.O57G8r4R4/s/m82K86tE2');
 
 INSERT INTO wallets (balance, currency, user_id) VALUES (5000.00, 'PLN', 1);
 INSERT INTO wallets (balance, currency, user_id) VALUES (100.00, 'PLN', 2);
