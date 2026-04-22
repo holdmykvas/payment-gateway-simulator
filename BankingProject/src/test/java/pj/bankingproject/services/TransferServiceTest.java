30f599526675b936b43f519555ac1bb1cafd85e5
Nikita Pohrebniak <nikitanerf540@gmail.com>
2026-04-22 11:07:56 +0200

    Revert "Unsuccessful transfer test"
    This reverts commit c2ae1120ca90aac94c4d9d2742b28da678134160.

--- BankingProject/src/test/java/pj/bankingproject/services/TransferServiceTest.java
+++ BankingProject/src/test/java/pj/bankingproject/services/TransferServiceTest.java
@@ -4,8 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.BDDMockito.given;
 import java.util.Optional;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -13,9 +11,6 @@ import pj.bankingproject.entities.User;
 import pj.bankingproject.entities.Wallet;
 import pj.bankingproject.repositories.TransferRepository;
 import pj.bankingproject.repositories.WalletRepository;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
 
 
 import java.math.BigDecimal;
@@ -37,55 +32,15 @@ public class TransferServiceTest {
                 .balance(BigDecimal.valueOf(1000))
                 .currency("Zloty")
                 .build();
-
         Wallet destinationWallet = Wallet.builder()
                 .id(2L)
                 .user(new User())
                 .balance(BigDecimal.valueOf(500))
                 .currency("Zloty")
                 .build();
-
         BigDecimal amount = BigDecimal.valueOf(200);
         given(walletRepository.findById(sourceWallet.getId())).willReturn(Optional.of(sourceWallet));
         given(walletRepository.findById(destinationWallet.getId())).willReturn(Optional.of(destinationWallet));
         transferService.transferMoney(sourceWallet.getId(),destinationWallet.getId(),amount);
-
-        //checking if balances changed
-        assertEquals(BigDecimal.valueOf(800), sourceWallet.getBalance());
-        assertEquals(BigDecimal.valueOf(700), destinationWallet.getBalance());
-
-        //checking if the repository actually called .save()
-        verify(walletRepository, times(1)).save(sourceWallet);
-        verify(walletRepository, times(1)).save(destinationWallet);
-    }
-
-    @Test
-    void transferMoneyUnsuccessful(){
-        Wallet sourceWallet = Wallet.builder()
-                .id(1L)
-                .user(new User())
-                .balance(BigDecimal.valueOf(100))
-                .currency("Zloty")
-                .build();
-
-        Wallet destinationWallet = Wallet.builder()
-                .id(2L)
-                .user(new User())
-                .balance(BigDecimal.valueOf(500))
-                .currency("Zloty")
-                .build();
-
-        BigDecimal amount = BigDecimal.valueOf(2000);
-        given(walletRepository.findById(1L)).willReturn(Optional.of(sourceWallet));
-        given(walletRepository.findById(2L)).willReturn(Optional.of(destinationWallet));
-
-        //this method will crash so need to wrap it
-        assertThrows(IllegalArgumentException.class, () -> {
-            transferService.transferMoney(1L,2L,amount);
-        });
-
-        //verifications which are not touched by crashed method
-        verify(walletRepository, never()).save(any());
-        verify(transferRepository, never()).save(any());
     }
 }
